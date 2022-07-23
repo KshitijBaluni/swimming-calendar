cd38fce01c1248f49306ed87c0a3b9e74fd94d8c
Kshitij Baluni <kbaluni@auracompany.com>
2022-07-23 14:54:35 +0530

    initial commit.

---
+++ src/main/java/com/example/swimmingcallender/SwimmingCalenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.swimmingcallender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimmingCalenderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SwimmingCalenderApplication.class, args);
+  }
+
+}
