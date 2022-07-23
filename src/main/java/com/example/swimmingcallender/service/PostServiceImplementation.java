1d793f27a8d14a3d3f3c32ff22ba21d3424885a5
Kshitij Baluni <kbaluni@auracompany.com>
2022-07-23 21:33:13 +0530

    created dummy post service.

---
+++ src/main/java/com/example/swimmingcallender/service/PostServiceImplementation.java
@@ -0,0 +1,38 @@
+package com.example.swimmingcallender.service;
+
+import com.example.swimmingcallender.domain.Post;
+
+import java.util.List;
+
+/**
+ * Post Service CRUD Implementation.
+ *
+ * @author kshitijbaluni
+ * @since 23 July 2022
+ */
+public class PostServiceImplementation implements PostService{
+  @Override
+  public List<Post> getPosts() {
+    return null;
+  }
+
+  @Override
+  public Post getPost(long id) {
+    return null;
+  }
+
+  @Override
+  public void createPost(Post post) {
+
+  }
+
+  @Override
+  public void updatePost(Post post) {
+
+  }
+
+  @Override
+  public void deletePost(long id) {
+
+  }
+}
