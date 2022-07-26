9b025a1d56cfd9b8fdabbe45341568177dd0806f
Kshitij Baluni <kbaluni@auracompany.com>
2022-07-26 11:29:34 +0530

    updates for jpa.

--- src/main/java/com/example/swimmingcalendar/repository/PostRepository.java
+++ src/main/java/com/example/swimmingcalendar/repository/PostRepository.java
@@ -1,7 +1,7 @@
 package com.example.swimmingcalendar.repository;
 
 import com.example.swimmingcalendar.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends CrudRepository<Post, Long> {
 }
